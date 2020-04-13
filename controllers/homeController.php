5a084d3d0ab2ad1834b96be2c412aa9f0cdf1830
Andre Motta <iandremotta@gmail.com>
2020-04-13 14:09:45 -0300

    'Finalizado'

--- controllers/homeController.php
+++ controllers/homeController.php
@@ -3,12 +3,8 @@
 class homeController extends controller {
 
     public function index(){
-        $anuncios = new Anuncios();
-        $usuarios = new Usuarios();
         $dados = array(
-            'quantidade' =>$anuncios->getQuantidade(),
-            'nome' => $usuarios->getNome(),
-            'idade' => $usuarios->getIdade()
+            
         );
         $this->loadTemplate('home', $dados);
     }   
--- views/home.php
+++ views/home.php
@@ -1,3 +1,1 @@
 <h1>Página de home</h1>
-<h2>Você tem <?php echo $quantidade ?> anuncios</h2>
-<h2>Seu nome é: <?php  echo $nome. ' e você tem '.$idade .' anos'?></h2>
