eccebff26f2c746dbc8298d5273fe9dfa024703e
André Motta <40709857+iandremotta@users.noreply.github.com>
2020-04-15 18:01:57 -0300

    Update Core.php

--- core/Core.php
+++ core/Core.php
@@ -27,6 +27,11 @@
     $currentController = 'homeController'; 
     $currentAction = 'index';
     }
+        
+    if(!file_exists('controllers/'.$currentController.'.php') || !method_exists($currentController, $currentAction)){
+        $currentController = 'notfoundController';
+        $currentAction = 'index';
+    }
     $controller = new $currentController(); 
     call_user_func_array(array($controller, $currentAction), $params);
     
